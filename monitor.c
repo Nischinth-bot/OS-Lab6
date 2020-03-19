7153f9795fe4dbdfe1651221df283278cad7a427
Nischinth Murari <murarins@appstate.edu>
2020-03-18 23:01:01 -0400

    Work begins

--- monitor.c
+++ monitor.c
@@ -5,22 +5,26 @@
 #include <assert.h>
 #include <dirent.h>
 #include <math.h>
-
 #include "fileHandle.h"
+#include "config.h"
 
 static void printUsage();
 static void parseArgs(int argc, char * argv[], char ** target, int * threads);
-
+struct arg_struct{
+    int tid;
+    int numDirs;
+    char ** dirs;
+};
 /*
  * printUsage
  * prints the usage information and exits
  */
 void printUsage()
 {
-   printf("usage: monitor -d <directory> -t <#threads>\n");
-   printf("-d <directory> - directory to monitor\n");
-   printf("-t <#threads> - number of threads doing the monitoring\n");
-   exit(1);
+    printf("usage: monitor -d <directory> -t <#threads>\n");
+    printf("-d <directory> - directory to monitor\n");
+    printf("-t <#threads> - number of threads doing the monitoring\n");
+    exit(1);
 }
 
 /*
@@ -37,19 +41,26 @@ void printUsage()
  */
 void parseArgs(int argc, char * argv[], char ** target, int * threads)
 {
-   int i;
-   if (argc != 5) printUsage();
-   for (i = 1; i < argc; i+=2)
-   {
-      if (strcmp("-d", argv[i]) == 0)
-      {
-         (*target) = strdup(argv[i+1]);
-      } else if (strcmp("-t", argv[i]) == 0)
-      {
-         (*threads) = atoi(argv[i + 1]);
-      } else
-         printUsage();
-   }
+    int i;
+    if (argc != 5) printUsage();
+    for (i = 1; i < argc; i+=2)
+    {
+        if (strcmp("-d", argv[i]) == 0)
+        {
+            (*target) = strdup(argv[i+1]);
+        } else if (strcmp("-t", argv[i]) == 0)
+        {
+            (*threads) = atoi(argv[i + 1]);
+        } else
+            printUsage();
+    }
+}
+
+
+/**
+ *
+ */
+void threadMonitor(void * arg){
 }
 
 /*
@@ -62,21 +73,49 @@ void parseArgs(int argc, char * argv[], char ** target, int * threads)
  */
 int main (int argc, char *argv[])
 {
-   char * target;
-   int i, nThreads;
-   char ** dirs;
-   int dirCt; 
+    char * target;
+    int i, nThreads;
+    char ** dirs;
+    int dirCt; 
 
-   parseArgs(argc, argv, &target, &nThreads);
-   dirCt = getDirectoryCount(target);
-   if (nThreads > dirCt) nThreads = dirCt;
-   
-   dirs = (char **) malloc(sizeof(char *) * dirCt);
+    parseArgs(argc, argv, &target, &nThreads);
+    dirCt = getDirectoryCount(target);
+    if (nThreads > dirCt) nThreads = dirCt;   
+    dirs = (char **) malloc(sizeof(char *) * dirCt);
+    getDirectoryList(target, dirs, dirCt);
+    for (i = 0; i < dirCt; i++) printf("dirs[%d] = %s\n", i, dirs[i]); 
 
-   getDirectoryList(target, dirs, dirCt);
-   for (i = 0; i < dirCt; i++) printf("dirs[%d] = %s\n", i, dirs[i]); 
 
-   return 0;
+    /**Code for threading**/
+    pthread_t threads[nThreads]; //Array of threads.
+    struct arg_struct args[nThreads];
+    int startIndex = 0;
+    int endIndex = 0;
+    int eachThread = dirCt/nThreads; //Number of directories per thread.
+    int dirsRemaining = dirCt % nThreads;
+
+    for (i = 0; i < nThreads; i ++){
+        endIndex = startIndex + eachThread - 1;
+          if(i == nThreads - 1 && dirsRemaining > 0) {
+            eachThread += dirsRemaining; //Accomadate remaining threads, if they exist 
+            endIndex += dirsRemaining; //Change the index for iteration
+        }
+        char * threadDirs[eachThread]; //Array of char pointers passed to each thread. 
+        int j = 0;
+        for(j = startIndex; j <= endIndex; j ++){
+            threadDirs[j - startIndex] = dirs[j];
+        }
+        args[i].tid = i;
+        args[i].numDirs = eachThread;
+        args[i].dirs = threadDirs;
+        int k;
+        for(j = 0; j < eachThread; j ++){
+            printf("%d %s\n", i, threadDirs[j]); 
+        }
+        startIndex = endIndex + 1;
+    }
+
+    return 0;
 }
 
 
